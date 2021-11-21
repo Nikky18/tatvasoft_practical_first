93c0ead03620ce70f28fc835a50c4c266c076e69
Nikky18 <nikkyjain1998@gmail.com>
2021-11-21 13:08:05 +0700

    add square root validation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.AttributeSet
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
 import timber.log.Timber
@@ -21,11 +23,20 @@ class MainActivity : AppCompatActivity()  {
 
 
     private  fun initUiComponent(){
-
         binding.submitBtn.setOnClickListener {
-            val gridNumber = binding.editText.text
-            Toast.makeText(this, gridNumber, Toast.LENGTH_SHORT).show()
+            var gridNumber = binding.editText.text.toString().toDouble()
+            Toast.makeText(this, "$gridNumber", Toast.LENGTH_SHORT).show()
             binding.editText.setText("")
+
+            val gridLayoutNumber = checkValidation(gridNumber)
+            Toast.makeText(this, "${gridLayoutNumber}", Toast.LENGTH_SHORT).show()
         }
+    }
+
+
+    private fun checkValidation(gridNumber: Double): Double{
+        val square = Math.sqrt(gridNumber)
+        val florValue = Math.floor(square)
+        return if(square == florValue) florValue else 0.0
     }
 }
