011346e82b09d0f464bb58b6c6543c993e777672
Nikky18 <nikkyjain1998@gmail.com>
2021-11-21 12:47:55 +0700

    enable binding and take user's input

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUiComponent()
+    }
+
+
+    private  fun initUiComponent(){
+
+        binding.submitBtn.setOnClickListener {
+            val gridNumber = binding.editText.text
+            Toast.makeText(this, gridNumber, Toast.LENGTH_SHORT).show()
+            binding.editText.setText("")
+        }
     }
 }
